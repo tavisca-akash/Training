f58ebb2484dee5f15d04af09c0fbbc64d9dfb10e
unknown <abhandwalkar@TRAINING3.internal.tavisca.com>
2015-07-11 18:10:15 +0530

    currency convertor is commited

---
+++ 01OperatorOverloading/OperatorOverloading.interfaces/IExchangeRateProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading.interfaces
+{
+    public interface IExchangeRateProvider
+    {
+      double GetExchangeRate(string sourceCurrency, string targetCurrency);
+    }
+}
