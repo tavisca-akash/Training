a57ac5d9ef947f3434e461e64040ddaebf5a8388
unknown <abhandwalkar@TRAINING3.internal.tavisca.com>
2015-07-15 13:56:38 +0530

    deleted OperatorOverloading branch

---
+++ Tavisca.WCF/Tavisca.Calculator.Implementation/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Tavisca.WCF.Services;
+
+namespace Tavisca.WCF.Implementation
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Calculator : ICalculator
+    {
+        public int Addition(int firstNumber, int secondNumber)
+        {
+            int answer = firstNumber + secondNumber;
+            return answer;
+        }
+        public int Subtraction(int firstNumber, int secondNumber)
+        {
+            int answer = firstNumber - secondNumber;
+            return answer;
+        }
+        public int Multiplication(int firstNumber, int secondNumber)
+        {
+            int answer = firstNumber * secondNumber;
+            return answer;
+        }
+        public int Division(int firstNumber, int secondNumber)
+        {
+            int answer = firstNumber - secondNumber;
+            return answer;
+        }
+    }
+}
---
+++ Tavisca.WCF/Tavisca.Calculator.Services/ICalculator.cs
@@ -0,0 +1,29 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Tavisca.WCF.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        int Addition(int firstNumber, int secondNumber);
+
+        [OperationContract]
+        int Subtraction(int firstNumber, int secondNumber);
+
+        [OperationContract]
+        int Multiplication(int firstNumber, int secondNumber);
+
+        [OperationContract]
+        int Division(int firstNumber, int secondNumber);
+
+    }
+
+
+}
