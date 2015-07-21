21deb6ad71f8bed19bf4e5a987499ef92c597cdd
unknown <abhandwalkar@TRAINING3.internal.tavisca.com>
2015-07-21 09:18:02 +0530

    bouncing ball with view is updated

---
+++ Bouncing ball/view/ball.js
@@ -0,0 +1,20 @@
+﻿
+window.ball = window.ball || {
+    xPosition: 0,
+    yPosition: 0,
+    dx: 10,
+    dy: 10
+};
+
+ball.bounce = function ()
+{
+    var element = document.getElementById('circle');
+    element.style.left = ball.xPosition + "px";
+    element.style.top = ball.yPosition + "px";
+    ball.container();
+    ball.xPosition += ball.dx;
+    ball.yPosition += ball.dy;
+}
+window.ball.start = function () {
+    setInterval(ball.bounce, 10);
+}
---
+++ Bouncing ball/view/container.js
@@ -0,0 +1,19 @@
+﻿window.ball = window.ball || {}
+
+ball.container = function () {
+
+    var height = (window.innerHeight - 50);
+    var width = (window.innerWidth - 50);
+    if (ball.yPosition >= height) {
+        ball.dy = -10;
+    }
+    if (ball.xPosition >= width) {
+        ball.dx = -10;
+    }
+    if (ball.yPosition <= 0) {
+        ball.dy = 10;
+    }
+    if (ball.xPosition <= 0) {
+        ball.dx = 10;
+    }
+}
