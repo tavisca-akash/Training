391c8f94c457f4f6be1bd3a4e405eba0d7a3f5f0
unknown <abhandwalkar@TRAINING3.internal.tavisca.com>
2015-08-09 21:06:43 +0530

    First commit of mvc

---
+++ Mvc__EMS/Mvc__EMS/Controllers/HRController.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc__EMS.Models;
+using RollBasedAccess;
+
+
+namespace Mvc__EMS.Controllers
+{
+    public class HRController : Controller
+    {
+        //
+        // GET: /HR/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GotoAddEmployee(AddEmployee model)
+        {
+          
+            return View("AddEmployeeView");
+           
+        }
+        public ActionResult SaveEmployee(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeResponse.Save(employee);
+                return View("AddEmployeeView");
+            }
+            else
+            {
+                return RedirectToAction("LoginPage","Login");
+            }
+        }
+        public ActionResult HRProfile(Employee employee)
+        {
+
+            
+                return View("HRProfileView");
+            
+        }
+        public ActionResult AddRemark(AddEmployee model)
+        {
+            GetAllEmployee response = GetAllEmployee.GetAllEmployeeDetails();
+            
+
+                List<SelectListItem> empList = new List<SelectListItem>();
+                foreach (var employee in response.AllEmployeeList)
+                {
+                    empList.Add(new SelectListItem { Value = employee.Id, Text = employee.FirstName + "  " + employee.LastName });
+                  
+                }
+                ViewData["EmpList"] = empList;
+              
+            
+            return View("AddRemarkView");
+        }
+        public ActionResult SaveRemark(AddRemark model)
+        {
+            Remark remark = new Remark();
+            string id = Request["Employee"];
+            remark.Text = model.Remark;
+            remark.CreateTimeStamp = Convert.ToDateTime(DateTime.UtcNow);
+            RemarkResponse.AddRemark(id, remark);
+            return View("HRProfileView");
+        }
+
+
+        public ActionResult GetEmployees(EmployeeModel model)
+        {
+            EmployeeModel emp = new EmployeeModel();
+            int id = int.Parse(TempData["Id"].ToString());
+            var response =EmployeeResponse.GetAllRemarks(id,2);
+            List<EmployeeModel> listEmployee = new List<EmployeeModel>();
+
+            int totalPages = 2;
+            int pageSize = 2;
+
+            foreach (var remark in response.Employee.Remarks)
+            {
+                emp = new EmployeeModel();
+                emp.Text = remark.Text;
+                emp.CreateTimeStamps = remark.CreateTimeStamp.ToString();
+                listEmployee.Add(emp);
+            }
+            ViewBag.TotalRows = pageSize;
+            ViewBag.PageSize = totalPages;
+            var data = listEmployee;
+            return View(data);
+        }
+        
+    }
+}
---
+++ Mvc__EMS/Mvc__EMS/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc__EMS.Models;
+using RollBasedAccess;
+using System;
+using WebMatrix.WebData;
+
+
+namespace Mvc__EMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoginPage(Login model)
+        {
+
+            return View("LoginView");
+        }
+        [HttpPost ]
+        public ActionResult LoginPage1(Login model)
+        {
+           
+            Credentials credential = new Credentials();
+            credential.EmailId = model.UserName;
+            credential.Password = model.Password;
+            var res=Credentials.Authenticate(credential);
+            if (res.Status.StatusCode == "200")
+            {
+
+                HttpCookie cookie = new HttpCookie("Cookie");
+                cookie["UserName"] = credential.EmailId;
+                cookie["Title"] = res.Employee.Title;
+                cookie.Expires.AddDays(30);
+                HttpContext.Response.Cookies.Add(cookie);
+
+                ViewBag.MyCookie = cookie;
+                
+                    if (string.Equals(res.Employee.Title, "Hr", StringComparison.CurrentCultureIgnoreCase))
+                        return RedirectToAction("HRProfile", "HR");
+                    else
+                    {
+                        TempData["Id"] = res.Employee.Id;
+                        return RedirectToAction("GetEmployees", "HR");
+                    }
+                
+            }
+            return View("LoginView");
+        }
+    }
+}
